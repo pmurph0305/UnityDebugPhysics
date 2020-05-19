58791e2e739201f773bbaec2784fe9110337817a
unknown <pmurph0305@gmail.com>
2020-05-19 13:35:24 -0400

    Overlap Documentation

--- DebugPhysics/DebugPhysicsOverlap.cs
+++ DebugPhysics/DebugPhysicsOverlap.cs
@@ -5,6 +5,16 @@ using UnityEngine;
 // Overlap box, sphere, capsule.
 public static partial class DebugPhysics
 {
+
+  /// <summary>
+  /// Compute and returns an array of colliders touching or within the box
+  /// </summary>
+  /// <param name="center">Cetner of the box</param>
+  /// <param name="halfExtents">Half of the size of the box in each dimension</param>
+  /// <param name="orientation">Rotation of the box</param>
+  /// <param name="layerMask">A layer mask used to selectively ignore colliders</param>
+  /// <param name="queryTriggerInteraction">Specifies whether this query should hit triggers</param>
+  /// <returns>An array with all colliders touching or within the box</returns>
   public static Collider[] OverlapBox(Vector3 center, Vector3 halfExtents, Quaternion orientation,
     int layerMask = Physics.AllLayers, QueryTriggerInteraction queryTriggerInteraction = QueryTriggerInteraction.UseGlobal)
   {
@@ -24,12 +34,27 @@ public static partial class DebugPhysics
     return colliders;
   }
 
-
+  /// <summary>
+  /// Compute and returns an array of colliders touching or within the box
+  /// </summary>
+  /// <param name="center">Cetner of the box</param>
+  /// <param name="halfExtents">Half of the size of the box in each dimension</param>
+  /// <returns>An array with all colliders touching or within the box</returns>
   public static Collider[] OverlapBox(Vector3 center, Vector3 halfExtents)
   {
     return OverlapBox(center, halfExtents, Quaternion.identity);
   }
 
+  /// <summary>
+  /// Compute and stores an array of colliders touching or within the box
+  /// </summary>
+  /// <param name="center">Cetner of the box</param>
+  /// <param name="halfExtents">Half of the size of the box in each dimension</param>
+  /// <param name="results">The buffer to store results in</param>
+  /// <param name="orientation">Rotation of the box</param>
+  /// <param name="layerMask">A layer mask used to selectively ignore colliders</param>
+  /// <param name="queryTriggerInteraction">Specifies whether this query should hit triggers</param>
+  /// <returns>The amount of colliders stored in the buffer</returns>
   public static int OverlapBoxNonAlloc(Vector3 center, Vector3 halfExtents, Collider[] results, Quaternion orientation,
     int layerMask = Physics.AllLayers, QueryTriggerInteraction queryTriggerInteraction = QueryTriggerInteraction.UseGlobal)
   {
@@ -49,12 +74,27 @@ public static partial class DebugPhysics
     return val;
   }
 
+  /// <summary>
+  /// Compute and stores an array of colliders touching or within the box
+  /// </summary>
+  /// <param name="center">Cetner of the box</param>
+  /// <param name="halfExtents">Half of the size of the box in each dimension</param>
+  /// <param name="results">The buffer to store results in</param>
+  /// <returns>The amount of colliders stored in the buffer</returns>
   public static int OverlapBoxNonAlloc(Vector3 center, Vector3 halfExtents, Collider[] results)
   {
     return OverlapBoxNonAlloc(center, halfExtents, results, Quaternion.identity);
   }
 
-
+  /// <summary>
+  ///  Computes and returns an array of colliders touching or within the capsule.
+  /// </summary>
+  /// <param name="point0">Center of the sphere at the start of the capsule</param>
+  /// <param name="point1">Center of the sphere at the end of the capsule</param>
+  /// <param name="radius">Radius of the capsule</param>
+  /// <param name="layerMask">A layer mask used to selectively ignore colliders</param>
+  /// <param name="queryTriggerInteraction">Specifies whether this query should hit triggers</param>
+  /// <returns>An array with all colliders touching or within the capsule</returns>
   public static Collider[] OverlapCapsule(Vector3 point0, Vector3 point1, float radius,
     int layerMask = Physics.AllLayers, QueryTriggerInteraction queryTriggerInteraction = QueryTriggerInteraction.UseGlobal)
   {
@@ -74,6 +114,16 @@ public static partial class DebugPhysics
     return colliders;
   }
 
+  /// <summary>
+  /// Computes and stores an array of colliders touching or within the capsule.
+  /// </summary>
+  /// <param name="point0">Center of the sphere at the start of the capsule</param>
+  /// <param name="point1">Center of the sphere at the end of the capsule</param>
+  /// <param name="radius">Radius of the capsule</param>
+  /// <param name="results">Buffer to store results into</param>
+  /// <param name="layerMask">A layer mask used to selectively ignore colliders</param>
+  /// <param name="queryTriggerInteraction">Specifies whether this query should hit triggers</param>
+  /// <returns>The amount of colliders stored in the buffer</returns>
   public static int OverlapCapsuleNonAlloc(Vector3 point0, Vector3 point1, float radius, Collider[] results,
     int layerMask = Physics.AllLayers, QueryTriggerInteraction queryTriggerInteraction = QueryTriggerInteraction.UseGlobal)
   {
@@ -93,6 +143,14 @@ public static partial class DebugPhysics
     return val;
   }
 
+  /// <summary>
+  /// Computes and returns an array of colliders touching or within the sphere.
+  /// </summary>
+  /// <param name="position">Center of the sphere</param>
+  /// <param name="radius">Radius of the sphere</param>
+  /// <param name="layerMask">A layer mask used to selectively ignore colliders</param>
+  /// <param name="queryTriggerInteraction">Specifies whether this query should hit triggers</param>
+  /// <returns>An array with all colliders touching or within the sphere</returns>
   public static Collider[] OverlapSphere(Vector3 position, float radius,
     int layerMask = Physics.AllLayers, QueryTriggerInteraction queryTriggerInteraction = QueryTriggerInteraction.UseGlobal)
   {
@@ -112,6 +170,15 @@ public static partial class DebugPhysics
     return colliders;
   }
 
+  /// <summary>
+  /// Computes and stores the amount of colliders touching or within the sphere.
+  /// </summary>
+  /// <param name="position">Center of the sphere</param>
+  /// <param name="radius">Radius of the sphere</param>
+  /// <param name="results">Buffer to store results into</param>
+  /// <param name="layerMask">A layer mask used to selectively ignore colliders</param>
+  /// <param name="queryTriggerInteraction">Specifies whether this query should hit triggers</param>
+  /// <returns>The amount of colliders stored in the buffer</returns>
   public static int OverlapSphereNonAlloc(Vector3 position, float radius, Collider[] results,
     int layerMask = Physics.AllLayers, QueryTriggerInteraction queryTriggerInteraction = QueryTriggerInteraction.UseGlobal)
   {
@@ -130,5 +197,4 @@ public static partial class DebugPhysics
     }
     return val;
   }
-
 }
