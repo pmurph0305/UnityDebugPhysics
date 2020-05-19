d76d62f4d6247de584dc7a86ecf3ce89b1818e70
unknown <pmurph0305@gmail.com>
2020-05-19 13:56:15 -0400

    Check documentation

--- DebugPhysics/DebugPhysicsCheck.cs
+++ DebugPhysics/DebugPhysicsCheck.cs
@@ -6,6 +6,15 @@ using UnityEngine;
 public static partial class DebugPhysics
 {
 
+  /// <summary>
+  /// Checks if any colliders overlap a box volume in world space
+  /// </summary>
+  /// <param name="center">Center of the box</param>
+  /// <param name="halfExtents">Half the size of the box in each dimension</param>
+  /// <param name="orientation">Rotation of the box</param>
+  /// <param name="layerMask">A layer mask used to selectively ignore colliders</param>
+  /// <param name="queryTriggerInteraction">Specifies whether this query should hit triggers</param>
+  /// <returns>True if any colliders are touching or within the provided box</returns>
   public static bool CheckBox(Vector3 center, Vector3 halfExtents,
     Quaternion orientation, int layermask = Physics.DefaultRaycastLayers,
     QueryTriggerInteraction queryTriggerInteraction = QueryTriggerInteraction.UseGlobal)
@@ -22,12 +31,26 @@ public static partial class DebugPhysics
     }
   }
 
+  /// <summary>
+  /// Checks if any colliders overlap a box volume in world space
+  /// </summary>
+  /// <param name="center">Center of the box</param>
+  /// <param name="halfExtents">Half the size of the box in each dimension</param>
+  /// <returns>True if any colliders are touching or within the provided box</returns>
   public static bool CheckBox(Vector3 center, Vector3 halfExtents)
   {
     return CheckBox(center, halfExtents, Quaternion.identity);
   }
 
 
+  /// <summary>
+  /// Checks if any colliders overlap a sphere volume in world space
+  /// </summary>
+  /// <param name="position">Center of the sphere in world space</param>
+  /// <param name="radius">Radius of the sphere</param>
+  /// <param name="layerMask">A layer mask used to selectively ignore colliders</param>
+  /// <param name="queryTriggerInteraction">Specifies whether this query should hit triggers</param>
+  /// <returns>True if any colliders are touching or within the provided sphere</returns>
   public static bool CheckSphere(Vector3 position, float radius, int layerMask = Physics.DefaultRaycastLayers,
     QueryTriggerInteraction queryTriggerInteraction = QueryTriggerInteraction.UseGlobal)
   {
@@ -43,6 +66,17 @@ public static partial class DebugPhysics
     }
   }
 
+
+
+  /// <summary>
+  /// Checks if any colliders overlap a capsule volume in world space
+  /// </summary>
+  /// <param name="start">Center of the sphere at the start of the capsule</param>
+  /// <param name="end">Center of the sphere at the end of the capsule</param>
+  /// <param name="radius">Radius of the capsule</param>
+  /// <param name="layerMask">A layer mask used to selectively ignore colliders</param>
+  /// <param name="queryTriggerInteraction">Specifies whether this query should hit triggers</param>
+  /// <returns>True if any colliders are touching or within the provided capsule</returns>
   public static bool CheckCapsule(Vector3 start, Vector3 end, float radius, int layermask = Physics.DefaultRaycastLayers,
     QueryTriggerInteraction queryTriggerInteraction = QueryTriggerInteraction.UseGlobal)
   {
